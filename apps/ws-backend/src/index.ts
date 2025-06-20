0db46717304c12d437768363bee78131cca47f8f
SahuUsha <sahuur@rknec.edu>
2025-06-20 20:27:15 +0530

    feat(ws-backend): implement chat functionality and user management

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,44 @@
-import { WebSocketServer } from 'ws'
+import { WebSocket, WebSocketServer } from 'ws'
 import jwt, { JwtPayload } from "jsonwebtoken"
 // import { JWT_SECRETE } from '@repo/backend-common/config';
 import { JWT_SECRET } from "./config";
+import {prismaClient} from "@repo/db"
 
 const wss = new WebSocketServer({ port: 8080 });
+
+
+// to managae state in backend -- we can user redux-tool and other
+// but here we are using array method not good but okay easy to understand
+
+interface User {
+  ws : WebSocket,
+  rooms: string[],
+  userId : string
+}
+
+const users : User[] = [];
+
+function checkUser(token:string) : string | null {
+try {
+    const decoded = jwt.verify(token,JWT_SECRET)
+  
+    if(typeof decoded == "string"){
+      return null;
+    }
+  
+  
+    if(!decoded || !decoded.userId){
+      return null
+    }
+  
+  
+    return decoded.userId
+} catch (error) {
+  console.log("error : ",error)
+  return null
+}
+
+}
 
 wss.on('connection', function connection(ws, request) {
   ws.on('error', console.error);
@@ -17,18 +52,73 @@ wss.on('connection', function connection(ws, request) {
    // ["ws://localhost:3000", "token = 123123"]
 
   const queryParams = new URLSearchParams(url.split('?')[1])
-  const token = queryParams.get('token') || ""
+  const token = queryParams.get('token') || "";
+  const userId = checkUser(token)
 
-   const deocoded = jwt.verify(token,JWT_SECRET)
+  if(userId==null){
+      ws.close()
+      return null
+  }
 
-   if(!deocoded  || !(deocoded as JwtPayload).userId){
-    ws.close();
-    return;
-   }
+  users.push({
+    userId,
+    rooms: [],
+    ws
+  })
 
 
-  ws.on('message', function message(data) {
-      ws.send('pong');
+
+
+
+  ws.on('message',async function message(data){
+
+    const parseData = JSON.parse(data as unknown as string)
+
+    if(parseData.type ==="join_room"){
+      const user = users.find(x=>x.ws==ws)
+      user?.rooms.push(parseData.roomId)
+    }
+      
+
+    if(parseData.type === "leave_room"){
+       const user = users.find(x=>x.ws==ws)
+    
+       if(!user){
+        return
+       }
+
+      user.rooms = user?.rooms.filter(x=>x===parseData.room)
+    }
+    
+  
+    if(parseData.type === "chat"){
+      const roomId = parseData.roomId;
+      const message = parseData.message;
+
+       await  prismaClient.chat.create({
+      data:{
+        roomId,
+        message,
+        userId
+
+      }
+    })
+
+
+      users.forEach(user=>{
+        if(user.rooms.includes(roomId)){
+          user.ws.send(JSON.stringify({
+            type:"chat",
+            message:message,
+            roomId
+          }))
+        }
+      })
+    }
+    
+
+
+
   });
-
+ 
 });
